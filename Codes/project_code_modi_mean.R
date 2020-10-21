ee28f5b2b9d2e665521610f6303876e8b16f9246
Philippine <philippine.renaudin@gmail.com>
2020-10-21 11:51:20 +0200

    Update project_code_modi_mean.R

--- Codes/project_code_modi_mean.R
+++ Codes/project_code_modi_mean.R
@@ -111,38 +111,38 @@ FMacp3d(PCA.res=acp_moy2, comp=1:3, group=as.factor(data[,4]), plotVars = FALSE,
 library(umap)
 
 Umap.data=umap(data[,-c(1,4)])
-plot(Umap.data$layout, col = data[,4]) # différence de zone
+plot(Umap.data$layout, col = data[,4], xlab = 'Dim1',ylab = 'Dim2', main = "Paramètres par défaut", pch = 20) # différence de zone
 
 #Paramètres par défaut
 umap_par=umap.defaults
 umap_par$n_neighbors=5
-umap_par$min_dist=0.25
+umap_par$min_dist=0.1
 umap_par$metric='euclidean'
 
 
 
 # umap_par$metric='cosine'
 # umap_par$n_neighbors=5
-# umap_par$min_dist=0.1
-# 
+# umap_par$min_dist=0.25
+ 
 # umap_par$metric='pearson2'
 # umap_par$min_dist=0.1
 # umap_par$n_neighbors=5
 
-umap_par$verbose=TRUE
+umap_par$verbose=F
 
 ## Test min_dist
 layout(matrix(c(1:6), nrow=2, ncol=3, byrow=TRUE))
 for (i in c(0.05, 0.1, 0.25, 0.5, 0.8, 0.99)){
   umap_par$min_dist=i
-  U=umap(df_features[,c(4,5,16,17,18,19,20,21,22)],config = umap_par)
-  plot(U$layout,col=df_features[,14],xlab = 'Dim1',ylab = 'Dim2',pch=20)
+  U=umap(data[,-c(1,4)],config = umap_par)
+  plot(U$layout,col=data[,4],xlab = 'Dim1',ylab = 'Dim2', main = paste("min_dist = ",i), pch = 16)
 }
 
 ## Test n_neighbors
 layout(matrix(c(1:6), nrow=2, ncol=3, byrow=TRUE))
 for (i in c(2, 5, 10, 20, 50, 100)){
   umap_par$n_neighbors=i
-  U=umap(df_features[,c(4,5,16,17,18,19,20,21,22)],config = umap_par,xlab = 'Dim1',ylab = 'Dim2')
-  plot(U$layout,col=df_features[,14],pch=20)
+  U=umap(data[,-c(1,4)],config = umap_par)
+  plot(U$layout,col=data[,4],xlab = 'Dim1',ylab = 'Dim2', main = paste("nombre de ppv : ",i),pch=16)
 }
